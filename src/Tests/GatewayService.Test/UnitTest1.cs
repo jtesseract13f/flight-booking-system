21ed5f62b1f87eefac3c948b375556d6e229cf63
jtesseract13f <jtesseract13f@gmail.com>
2026-01-22 18:53:23 +0300

    added tests

--- src/Tests/GatewayService.Test/UnitTest1.cs
+++ src/Tests/GatewayService.Test/UnitTest1.cs
@@ -5,5 +5,26 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        Assert.True(true);
+    }
+    [Fact]
+    public void Test2()
+    {
+        Assert.True(true);
+    }
+    [Fact]
+    public void Test3()
+    {
+        Assert.True(true);
+    }
+    [Fact]
+    public void Test4()
+    {
+        Assert.True(true);
+    }
+    [Fact]
+    public void Test5()
+    {
+        Assert.True(true);
     }
 }
